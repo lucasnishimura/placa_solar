291bda933ef741a4274a87f7dca5fb40e56a1270
lucasnishimura <lucasnishimura@alphacode.com.br>
2019-09-17 19:00:13 -0300

    ajustes de mobile

--- js/main.js
+++ js/main.js
@@ -84,14 +84,14 @@ $(document).ready(function(){
         {
           breakpoint: 600,
           settings: {
-            slidesToShow: 3,
+            slidesToShow: 1,
             slidesToScroll: 1,
           }
         },
         {
           breakpoint: 480,
           settings: {
-            slidesToShow: 2,
+            slidesToShow: 1,
             slidesToScroll: 1,
           }
         }
@@ -102,7 +102,32 @@ $(document).ready(function(){
   $('.slick-install').slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 3
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
   });
 
   $('.slick-monitor').slick({
